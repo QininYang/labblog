677aaaa5d95afe1c8b76741fa393b20b72d407c0
hei6775 <913799761@qq.com>
2019-01-29 17:56:21 +0800

    uodate the description moudle
    Signed-off-by: hei6775 <913799761@qq.com>

--- src/components/DesCom.vue
+++ src/components/DesCom.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="des-main">
+    <h4 class="des-title">Description</h4>
     <div class="des-left"></div>
     <div class="des-right">
-      <div class="block">
-        <span class="demonstration">默认 Hover 指示器触发</span>
-        <el-carousel height="150px">
-          <el-carousel-item v-for="item in 4" :key="item">
-            <h3>{{ item }}</h3>
-          </el-carousel-item>
-        </el-carousel>
-      </div>
+      <template>
+        <div class="block">
+          <el-carousel height="250px">
+            <el-carousel-item v-for="item in 4" :key="item">
+              <h3>{{ item }}</h3>
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+      </template>
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: "description",
+    name: "descriptioncom",
     data() {
       return {};
     }
@@ -24,15 +26,44 @@
 </script>
 
 <style lang="scss" scoped>
+  .des-main {
+    width: 100%;
+    height: 302.6px;
+    background-color: cornsilk;
+  }
+  .des-title {
+    font-family: "Roboto", sans-serif;
+    font-weight: 300;
+    font-size: 20px;
+    color: #820000;
+    margin-top: 0px;
+    text-align: left;
+    border-bottom: 2px solid #820000;
+  }
+  .des-left {
+    display: inline-block;
+    left: 0px;
+    width: 50%;
+    height: 250px;
+    background-color: aqua;
+  }
+  .des-right {
+    display: inline-block;
+    right: 0px;
+    width: 50%;
+  }
   .el-carousel__item h3 {
     color: #da2a2a;
     font-size: 14px;
     opacity: 0.75;
-    line-height: 150px;
+    line-height: 250px;
     margin: 0;
   }
-  .des-right {
-    position: absolute;
-    right: 0px;
+  .el-carousel__item:nth-child(2n) {
+    background-color: #99a9bf;
+  }
+
+  .el-carousel__item:nth-child(2n + 1) {
+    background-color: #d3dce6;
   }
 </style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -8,7 +8,9 @@
     </div>
     <div class="container-highlights"></div>
     <div class="container-mainer"></div>
-    <div class="container-footer"></div>
+    <div class="container-footer">
+      <footer-com />
+    </div>
   </div>
 </template>
 
@@ -16,11 +18,13 @@
   // @ is an alias to /src
   import HeaderCom from "@/components/HeaderCom.vue";
   import DesCom from "@/components/DesCom.vue"
+  import FooterCom from "@/components/FooterCom.vue"
   export default {
     name: "home",
     components: {
       HeaderCom,
-      DesCom
+      DesCom,
+      FooterCom
     }
   };
 </script>
