2ef065b0db57f3c349907d3c2abeb46873bea538
hei6775 <913799761@qq.com>
2019-01-30 19:15:28 +0800

    coverage container fix
    Signed-off-by: hei6775 <913799761@qq.com>

--- src/components/CoverCom.vue
+++ src/components/CoverCom.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="cover-main">
-    <a class="cover-title" :href="item.url">{{ item.title }} </a>
+    <div>
+      <a class="cover-title" :href="item.url">{{ item.title }} </a>
+    </div>
     <div class="cover-date">{{ item.publish }}</div>
     <div class="cover-year">{{ item.year }}</div>
   </div>
@@ -25,8 +27,19 @@
   .cover-main {
     border-left: 5px solid red;
     margin-bottom: 20px;
-  }
-  .a {
-    text-align: left;
+    display: inline;
+    float: left;
+    .cover-title {
+      float: left;
+
+      text-align: left;
+      font-size: 18px;
+    }
+    .cover-date {
+      display: inline-block;
+    }
+    .cover-year {
+      display: inline-block;
+    }
   }
 </style>
